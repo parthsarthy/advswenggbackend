c3598e6800a14d5f3acacc37b1f081d914a10390
Parth Sarthy <parthsarthy@Parths-MacBook-Air.local>
2018-03-07 16:23:29 +0000

    DB Link added

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,18 @@
 /* NodeJs mongodb tutorial - insert update delete records */
 
-https://abc.com/fetchdata/add
-
 var express     = require('express');
 var router      = express.Router();
 var mongodb     = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
-var dburl = "mongodb://localhost:27017/onlinestore";
+var dburl = "mongodb://ec2-34-244-27-78.eu-west-1.compute.amazonaws.com:27018/admin";
 
 /* GET products listing. */
 router.get('/', function(req, res, next) {
   MongoClient.connect(dburl, function(err, db) {
     if(err) {  console.log(err); throw err;  }
     data = '';
-    db.collection('products').find().toArray(function(err, docs){
+    db.collection('CarBooking').find().toArray(function(err, docs){
       if(err) throw err;
       res.render('index.jade', {data: docs});
       db.close();
@@ -27,7 +25,7 @@ router.get('/fetchdata', function(req, res, next) {
    MongoClient.connect(dburl, function(err, db) {
     if(err) {  console.log(err); throw err;  }
     data = '';
-    db.collection('products').find({_id: new mongodb.ObjectID(id)}).toArray(function(err, docs){
+    db.collection('CarBooking').find({_id: new mongodb.ObjectID(id)}).toArray(function(err, docs){
       if(err) throw err;
       res.send(docs);
       db.close();
@@ -38,7 +36,7 @@ router.get('/fetchdata', function(req, res, next) {
 router.post('/add', function(req, res, next) {
   MongoClient.connect(dburl, function(err, db) {
     if(err) { throw err;  }
-    var collection = db.collection('products');
+    var collection = db.collection('CarBooking');
     var product = { from: req.body.from,to: req.body.to, seats: req.body.seats, price: req.body.price,time: req.body.time,repetition: req.body.repetition};
     collection.insert(product, function(err, result) {
     if(err) { throw err; }
@@ -50,7 +48,7 @@ router.post('/add', function(req, res, next) {
 router.post('/edit', function(req, res, next) {
   MongoClient.connect(dburl, function(err, db) {
     if(err) { throw err;  }
-    var collection = db.collection('products');
+    var collection = db.collection('CarBooking');
     var product = {'product_name': req.body.product_name, 'price': req.body.price, 'category': req.body.category};
     collection.update({'_id':new mongodb.ObjectID(req.body.id)}, {$set:{'from': req.body.from,'to': req.body.to, 'seats': req.body.seats, 'price': req.body.price,'time': req.body.time,'repetition': req.body.repetition}}, function(err, result) {
     if(err) { throw err; }
@@ -65,7 +63,7 @@ router.get('/delete', function(req, res, next) {
   MongoClient.connect(dburl, function(err, db) {
     if(err) { throw err;  }
 
-    db.collection('products', function(err, products) {
+    db.collection('CarBooking', function(err, products) {
       products.deleteOne({_id: new mongodb.ObjectID(id)});
       if (err){
          throw err;
